5a225484b90bed1a618781dbe21529c33014ff98
Dorian Bucaille <dorian.bucaille@orange.fr>
2022-01-09 00:09:23 +0100

    Added first post to database

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello world?");
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import microConfig from "./mikro-orm.config";
+
+const main = async () => {
+  const orm = await MikroORM.init(microConfig); // connect to database
+  await orm.getMigrator().up(); // run migrations
+  const post = orm.em.create(Post, { title: "This is my first post" }); // creates a post
+  await orm.em.persistAndFlush(post); // insert post to database
+};
+
+main().catch((err) => {
+  console.error(err);
+});
---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,21 @@
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import { MikroORM } from "@mikro-orm/core";
+import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config(); // retrieves keys and values defined in the .env file
+
+export default {
+  migrations: {
+    path: path.join(__dirname, "./migrations"), // path to the folder with migrations
+    pattern: /^[\w-]+\d+\.[tj]s$/, // regex pattern for the migration files
+  },
+  entities: [Post],
+  dbName: "raidite",
+  type: "postgresql",
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  debug: !__prod__, // debugs only when not in production
+} as Parameters<typeof MikroORM.init>[0];
+// as Parameters<typeof MikroORM.init>[0] allows for autocompletion according to MikroORM.init() fields.
