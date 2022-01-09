8043e9839b9c16a575667f853988fd43a6fae44d
Dorian Bucaille <dorian.bucaille@orange.fr>
2022-01-09 14:24:36 +0100

    Added post(id) query

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,11 +1,22 @@
 import { Post } from "../entities/Post";
 import { MyContext } from "src/types";
-import { Ctx, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Int, Query, Resolver } from "type-graphql";
 
 @Resolver()
 export class PostResolver {
+
+  // Return every post in the database.
   @Query(() => [Post])
   posts(@Ctx() ctx: MyContext): Promise<Post[]> {
     return ctx.em.find(Post, {});
+  }
+
+  // Return the post with the specified id.
+  @Query(() => Post, { nullable: true })
+  post(
+    @Arg("id", () => Int) id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    return em.findOne(Post, { _id: id });
   }
 }
