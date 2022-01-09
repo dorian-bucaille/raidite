4dcb1613dbf6fa7aaa7cb818393dffd5e83d909e
Dorian Bucaille <dorian.bucaille@orange.fr>
2022-01-09 14:31:22 +0100

    Added createPost(title) query

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,10 +1,9 @@
 import { Post } from "../entities/Post";
 import { MyContext } from "src/types";
-import { Arg, Ctx, Int, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Mutation, Query, Resolver } from "type-graphql";
 
 @Resolver()
 export class PostResolver {
-
   // Return every post in the database.
   @Query(() => [Post])
   posts(@Ctx() ctx: MyContext): Promise<Post[]> {
@@ -13,10 +12,18 @@ export class PostResolver {
 
   // Return the post with the specified id.
   @Query(() => Post, { nullable: true })
-  post(
-    @Arg("id", () => Int) id: number,
-    @Ctx() { em }: MyContext
-  ): Promise<Post | null> {
+  post(@Arg("id") id: number, @Ctx() { em }: MyContext): Promise<Post | null> {
     return em.findOne(Post, { _id: id });
+  }
+
+  // Add a post to the database.
+  @Mutation(() => Post)
+  async createPost(
+    @Arg("title") title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post> {
+    const post = em.create(Post, { title });
+    await em.persistAndFlush(post);
+    return post;
   }
 }
