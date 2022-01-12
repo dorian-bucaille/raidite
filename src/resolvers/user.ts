c03c7f33aea8d719a8df55f1043d9da41501a511
Dorian Bucaille <dorian.bucaille@orange.fr>
2022-01-12 18:07:32 +0100

    Register now logs users in automatically

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -55,7 +55,7 @@ export class UserResolver {
   @Mutation(() => UserResponse)
   async register(
     @Arg("options") options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext
+    @Ctx() { em, req }: MyContext
   ): Promise<UserResponse> {
     if (options.username.length < 3) {
       return {
@@ -100,6 +100,9 @@ export class UserResolver {
       }
       console.log("Error: ", err.message);
     }
+
+    // Store user session with a cookie (thus keeping them logged in)
+    req.session!.userId = user._id;
 
     return { user };
   }
