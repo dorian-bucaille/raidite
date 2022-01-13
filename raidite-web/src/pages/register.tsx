ef3408ea58a41e2853d5912d1ca66a28eee6a807
Dorian Bucaille <dorian.bucaille@orange.fr>
2022-01-13 20:25:20 +0100

    Added Formik to the register page

--- raidite-web/src/pages/register.tsx
+++ raidite-web/src/pages/register.tsx
@@ -1,9 +1,40 @@
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+} from "@chakra-ui/react";
+import { Form, Formik } from "formik";
 import React from "react";
+import { Wrapper } from "../components/Wrapper";
 
 interface registerProps {}
 
 const Register: React.FC<registerProps> = ({}) => {
-  return <div>Register page</div>;
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {(values, handleChanges) => (
+          <Form>
+            <FormControl>
+              <FormLabel htmlFor="username">Username</FormLabel>
+              <Input
+                id="username"
+                placeholder="Username"
+                onChange={handleChanges}
+              />
+              {/* <FormErrorMessage>{values.username}</FormErrorMessage> */}
+            </FormControl>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
 };
 
 export default Register;
