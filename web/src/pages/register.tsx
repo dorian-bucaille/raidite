6075dece666ecffb1b9852c12dc3f45269454681
Dorian Bucaille <dorian.bucaille@orange.fr>
2022-01-14 10:20:59 +0100

    Added register hook to call graphql

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -8,18 +8,35 @@ import {
 } from "@chakra-ui/react";
 import { Form, Formik } from "formik";
 import React from "react";
+import { useMutation } from "urql";
 import { InputField } from "../components/InputField";
 import { Wrapper } from "../components/Wrapper";
 
 interface registerProps {}
 
+const REGISTER_MUT = `
+mutation Register($username: String!, $password: String!) {
+  register(options: { username: $username, password: $password }) {
+    errors {
+      field
+      message
+    }
+    user {
+      _id
+      username
+    }
+  }
+}`;
+
 const Register: React.FC<registerProps> = ({}) => {
+  const [, register] = useMutation(REGISTER_MUT);
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
         onSubmit={(values) => {
           console.log(values);
+          register(values);
         }}
       >
         {({ isSubmitting }) => (
