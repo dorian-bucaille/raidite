5320a41c4c1854fd168ee744f757335c3d4716b7
Dorian Bucaille <dorian.bucaille@orange.fr>
2022-01-27 17:04:43 +0100

    Converted SQL query to a QueryBuilder

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -11,6 +11,7 @@ import {
   Resolver,
 } from "type-graphql";
 import argon2 from "argon2";
+import { EntityManager } from "@mikro-orm/postgresql";
 
 @InputType()
 class UsernamePasswordInput {
@@ -80,12 +81,19 @@ export class UserResolver {
     }
 
     const hashedPassword = await argon2.hash(options.password); // hash the plain password using argon2
-    const user = em.create(User, {
-      username: options.username,
-      password: hashedPassword,
-    });
+    let user;
     try {
-      await em.persistAndFlush(user);
+      const result = await (em as EntityManager)
+        .createQueryBuilder(User)
+        .getKnexQuery()
+        .insert({
+          username: options.username,
+          password: hashedPassword,
+          created_at: new Date(),
+          updated_at: new Date(),
+        })
+        .returning("*");
+      user = result[0];
     } catch (err) {
       // If duplicate username error
       if (err.code === "23505") {
