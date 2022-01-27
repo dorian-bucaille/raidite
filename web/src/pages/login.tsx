90f2bdba2cfc90c26f5f398e76fd8766804cea20
Dorian Bucaille <dorian.bucaille@orange.fr>
2022-01-27 17:14:45 +0100

    Login works!

--- web/src/pages/login.tsx
+++ web/src/pages/login.tsx
@@ -9,25 +9,23 @@ import {
 import { Form, Formik } from "formik";
 import { useRouter } from "next/router";
 import React from "react";
-import { useMutation } from "urql";
 import { InputField } from "../components/InputField";
 import { Wrapper } from "../components/Wrapper";
-import { useRegisterMutation } from "../generated/graphql";
+import { useLoginMutation } from "../generated/graphql";
 import { toErrorMap } from "../utils/toErrorMap";
-
 
 const Login: React.FC<{}> = ({}) => {
   const router = useRouter();
-  const [, register] = useRegisterMutation();
+  const [, login] = useLoginMutation();
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
         onSubmit={async (values, { setErrors }) => {
-          const response = await register(values);
-          if (response.data?.register.errors) {
-            setErrors(toErrorMap(response.data.register.errors));
-          } else if (response.data.register.user) {
+          const response = await login({options: values});
+          if (response.data?.login.errors) {
+            setErrors(toErrorMap(response.data.login.errors));
+          } else if (response.data.login.user) {
             router.push("/");
           }
         }}
@@ -53,7 +51,7 @@ const Login: React.FC<{}> = ({}) => {
               isLoading={isSubmitting}
               bgColor="orange"
             >
-              Register
+              Login
             </Button>
           </Form>
         )}
